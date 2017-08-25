a1f9b89f12b2f1de3267a3d8acee3e1e463a72b0
Eugene Papkou <30703998+EugenePapkou@users.noreply.github.com>
2017-08-25 12:57:07 +0300

    Update 3.2.cpp

--- 3.2.cpp
+++ 3.2.cpp
@@ -1,5 +1,6 @@
 /* Write a program for encrypting and decrypting messages using the specified method.
-Encrypt the text as follows: write a message into the matrix in a spiral counter-clockwise, starting from the outside. Write the result of line-by-line matrix reading into the string. The size of matrix depends on length of a message.
+Encrypt the text as follows: write a message into the matrix in a spiral counter-clockwise, starting from the outside. 
+Write the result of line-by-line matrix reading into the string. The size of matrix depends on length of a message.
 Decrypt the text.
 
 task_10.05.2010 */
